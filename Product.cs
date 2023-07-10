2bdfb98580d5d37c662f217a2260ab5131f81f96
Yegana Hasanova <yeganamha@code.edu.az>
2023-07-10 11:36:33 -0700

    Add categories

---
+++ Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Project
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public Category Categories { get; set; }  
+    }
+
+    public enum Category
+    { 
+        Water,
+        Fruit,
+        Vegetable,
+        Seafood,
+        cookies,
+        gastronomy
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
